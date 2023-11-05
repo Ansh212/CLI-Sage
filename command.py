b291652e350170eb1acdab3c5e4c5818d5fd086f
shreyansh jain <shreyanshj90.sj@gmail.com>
2023-11-05 09:58:05 +0530

    Final build

--- command.py
+++ command.py
@@ -1,8 +1,10 @@
 import openai
-import subprocess,os
+import subprocess
+import os
 import sys
 import signal
 import config
+
 from colorama import Fore, Back, Style
 
 
@@ -18,15 +20,22 @@ def signal_handler(signal, frame):
 openai.api_key = config.api_key
 
 
-def chat_with_gpt(prompt: str) -> str:
+def chat_with_gpt(prompt: str, conversation_history: list) -> str:
     generated_text = ""
+
+    messages = [
+        {"role": "system", "content": "You are a Linux command assistant, respond only with the command without explanation"}
+    ]
+
+    for turn in conversation_history:
+        messages.append({"role": "user", "content": turn[0]})
+        messages.append({"role": "assistant", "content": turn[1]})
+
+    messages.append({"role": "user", "content": prompt})
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a Linux command assistant, respond only with the command without explanation"},
-            {"role": "user", "content": prompt}
-        ],
+        messages=messages,
         stream=True
     )
 
@@ -34,25 +43,24 @@ def chat_with_gpt(prompt: str) -> str:
         for chunk in response:
             delta_content = chunk['choices'][0]['delta'].get('content', '')
             if delta_content:
-                # Print and append the generated content to generated_text variable
                 generated_text += delta_content
                 print(Style.BRIGHT + Fore.LIGHTCYAN_EX + delta_content + Style.RESET_ALL, end='', flush=True)
 
     except Exception as e:
         print(f"Error: {e}")
 
-    print()  # Move to the next line after streaming the response
+    print()
     return generated_text
 
-def color_print(text, color_code):
-    formatted_text = f"\033[{color_code}m {text} \033[0m"
-    print(formatted_text, end='', flush=True)  # Print without newline and flush the output
+# Rest of your code remains unchanged
 
 if __name__ == "__main__":
-    prev_res = ""
+    conversation_history = []  # List to store (prompt, response) tuples
     response = ""
 
     print(Style.BRIGHT + Fore.YELLOW + "Press (E)xecute to run commands and CTRL-C to exit" + Style.RESET_ALL)
+
+
     signal.signal(signal.SIGINT, signal_handler)
 
     try:
@@ -62,19 +70,15 @@ if __name__ == "__main__":
             if user_input.lower() in ['quit', 'exit', 'bye', 'q', 'x', 'b']:
                 break
 
-            if user_input.lower() == "e":
+            if user_input == "E":
                 # Execute the generated command
                 os.system(response)
-                print(continuous_line)
-                prev_res=""
-                continue
+                print(continuous_line)                
+                conversation_history.append((user_input, response))
+                continue 
 
-            if prev_res != "":
-                prev_res = "This was the previous command " + prev_res
-
-            response = chat_with_gpt(user_input + " " + prev_res)
-            prev_res = response
+            response = chat_with_gpt(user_input, conversation_history)
+            conversation_history.append((user_input, response))
 
     except KeyboardInterrupt:
-        print("okay")
-
+        print("Okay")
